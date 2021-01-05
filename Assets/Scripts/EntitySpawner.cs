2883e6c6dbaab2f12ba6c398aca5f43acf71103f
Lucky-Martin <luckymartinlc@gmail.com>
2021-01-05 21:10:05 +0200

    Init commit

---
+++ Assets/Scripts/EntitySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EntitySpawner : MonoBehaviour
+{
+    [Header("Entities")]
+    public GameObject entity;
+
+    [Header("Spawn time")]
+    public int minSpawnTime = 1;
+    public int maxSpawnTime = 5;
+
+    private bool shouldSpawn = true;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        while (shouldSpawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime + 1));
+            Instantiate(entity, transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public bool loadStartScene = false;
+    public int loadStartSceneDelay = 3;
+
+    private void Awake()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if (loadStartScene)
+        {
+            StartCoroutine(LoadStartingSceneCoroutine());
+        }
+    }
+
+    private IEnumerator LoadStartingSceneCoroutine()
+    {
+        yield return new WaitForSeconds(loadStartSceneDelay);
+        LoadStartScene();
+    }
+
+    private void LoadStartScene()
+    {
+        SceneManager.LoadScene("Start Scene");
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
